f0d758225b4d30bfb951407cee6cb0d2a0e96175
mikiya118 <maoyriukgfiamn811@hotmail.com>
2020-05-12 00:24:23 +0900

    ブルワリー詳細更新２（servises, views, main_content, sub_content）

--- apps/brewery/services.py
+++ apps/brewery/services.py
@@ -2,11 +2,11 @@
 import common.models as MODELS
 from django.db import transaction
 from core import settings as SETTING
+from operator import attrgetter
 import logging
+import pprint
 
 logger = logging.getLogger('app')
-
-#    beer_list = []
 
 def selectBreweryById(id):
     try:
@@ -17,18 +17,14 @@ def selectBreweryById(id):
 
 def selectBeerlistByBrewery(brewery):
     try:
-
-        print(brewery.id)
-
         beer_list = MODELS.Beer.objects.filter(brewery=brewery.id)
-#       beer_list = MODELS.Beer.objects.filter(brewery=brewery.id
         return beer_list
     except:
         return None
 
 def selectCommentListByBeer(beer):
     try:
-        comment_list = MODELS.Comment.objects.filter(beer=beer.id).order_by('registered_date').reverse()[0:20]
+        comment_list = MODELS.Comment.objects.filter(beer=beer.id).order_by('registered_date').reverse()
         return comment_list
     except:
         return None
@@ -38,8 +34,22 @@ def selectCommentListByBrewery(brewery):
         beer_list = selectBeerlistByBrewery(brewery)
         comment_list = []
         for beer in beer_list:
-#            comment_list = selectCommentListByBeer(beer)#上書きされるからだめ
             comment_list.extend(selectCommentListByBeer(beer))
+            if len(comment_list) > 20:
+                break
+        comment_list = sorted(comment_list, key=attrgetter('registered_date'), reverse=True)
+        comment_list = comment_list[:20]
         return comment_list
     except:
         return []
+
+def selectVenueListByBrewery(brewery):
+    try:
+        comment_list = selectCommentListByBrewery(brewery)
+        venue_list = []
+        for comment in comment_list:
+            venue_list.append(comment.venue.name)
+        venue_list = list(set(venue_list))
+        return venue_list
+    except:
+        return None
