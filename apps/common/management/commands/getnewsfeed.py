c2286460468be25fffea12f5336b6f2110ee3972
HiroMuller <ex.hrs.zell@gmail.com>
2020-05-19 22:38:39 +0900

    フィード取得処理追加

---
+++ apps/common/management/commands/getnewsfeed.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from core import configs as CONFIG
+import common.models as MODELS
+import common.services as COMMON_SERVICES
+import feedparser
+
+"""
+コマンドラインより $ python manage.py getnewsfeed　で実行可能。
+cronにより定期実行する。
+"""
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        urls = CONFIG.NEWS_FEED_URLS
+        MODELS.NewsFeed.objects.filter().delete()
+        for url in urls:
+            feedpars = feedparser.parse(url)
+            for feed in feedpars.entries:
+                entry = MODELS.NewsFeed()
+                entry.site_name = feedpars.feed['title']
+                entry.link = feed['link']
+                entry.title = feed['title']
+                entry.date = COMMON_SERVICES.parseDate(feed['updated_parsed'] or entry['published_parsed'])
+                try:
+                    entry.photo = feed['href']
+                except:
+                    pass
+                entry.save()
