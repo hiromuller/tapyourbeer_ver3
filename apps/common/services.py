dbc71cbdf91df4e33954eccdc1ea5db9c97639f9
HiroMuller <ex.hrs.zell@gmail.com>
2020-10-14 21:59:19 +0900

    サービス処理修正

--- apps/common/services.py
+++ apps/common/services.py
@@ -7,7 +7,6 @@ import logging
 import jaconv
 from datetime import datetime as dt
 from django.utils import timezone
-import user.services as USER_SERVICES
 
 logger = logging.getLogger('app')
 
@@ -336,12 +335,18 @@ def addVenueWish(user, venue):
 
 def addReply(user, input_text, comment_id):
     try:
-        comment = USER_SERVICES.selectCommentById(comment_id)
+        comment = selectCommentById(comment_id)
         reply_model = MODELS.Reply()
         reply_model.reply = input_text
         reply_model.user = user
         reply_model.comment = comment
         reply_model.save()
         return reply_model
+    except:
+        return None
+
+def selectCommentById(id):
+    try:
+        return MODELS.Comment.objects.get(id = id)
     except:
         return None
